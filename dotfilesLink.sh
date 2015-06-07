c6adefbeaaf0d543d8a50072ffdfc94067522cf1
y.urakawa <s08a2015@gmail.com>
2015-06-07 22:21:08 +0900

    del .zsh file

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,7 +7,5 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
