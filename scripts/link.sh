e192fcc61526f104d6d7016342d8fbe64e9f799f
y.urakawa <s08a2015@gmail.com>
2024-01-05 17:17:32 +0900

    add

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-for dotfile in ${SCRIPT_DIR}/.?* ; do
+for dotfile in ${SCRIPT_DIR}/.??* ; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
